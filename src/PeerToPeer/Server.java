5c7c63d4e690fa21d897b74d0a7f0907a3b06a58
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-06-06 10:19:53 +0200

    Fixed a Bug at calculating on defense

--- src/PeerToPeer/Server.java
+++ src/PeerToPeer/Server.java
@@ -80,10 +80,10 @@ public class Server
                         if(type.equals("?")) {
                             int attackPower = Integer.parseInt(input);
 
-                            Main.sword.decreaseCount(attackPower/10/3);
-                            Main.spear.decreaseCount(attackPower/10/3);
-                            Main.bow.decreaseCount(attackPower/10/3);
-                            Main.crossbow.decreaseCount(attackPower/10/3);
+                            Main.sword.decreaseCount(attackPower/4/3);
+                            Main.spear.decreaseCount(attackPower/4/3);
+                            Main.bow.decreaseCount(attackPower/4/3);
+                            Main.crossbow.decreaseCount(attackPower/4/3);
                         }
 
                         if(type.equals("$"))
